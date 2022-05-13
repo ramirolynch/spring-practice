8ee3a4b9c27b4d9728ee9e8d9796fde6d26c700f
ramirolynch <ramirolynch@yahoo.com>
2022-05-13 08:26:59 -0500

    added update crud operation

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -12,4 +12,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
     @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
+
+    
 }
